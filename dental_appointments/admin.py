5c03b47d6a7d05c3481f716b7d560e18ae60ba81
Rhay from WSL <javier.johnrhay@gmail.com>
2025-12-01 11:17:39 +0800

    Add admin configurations for DentalAppointment, Patient, Dentist, MedicalRecord, and Address models

--- dental_appointments/admin.py
+++ dental_appointments/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import DentalAppointment
 
-# Register your models here.
+
+@admin.register(DentalAppointment)
+class DentalAppointmentAdmin(admin.ModelAdmin):
+    list_display = (
+        "patient",
+        "dentist",
+        "date_of_appointment",
+        "location",
+        "appointment_status",
+        "created_at",
+    )
+    search_fields = (
+        "patient__user__username",
+        "patient__user__email",
+        "dentist__user__username",
+        "dentist__user__email",
+        "location",
+    )
+    list_filter = ("appointment_status", "date_of_appointment", "created_at")
+    readonly_fields = ("created_at", "updated_at", "created_by", "updated_by")
+    
+    def save_model(self, request, obj, form, change):
+        if not change:  # Creating new appointment
+            obj.created_by = request.user
+        obj.updated_by = request.user
+        super().save_model(request, obj, form, change)
--- dental_records/admin.py
+++ dental_records/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
+from .models import Patient, Dentist, MedicalRecord, Address
 
-# Register your models here.
+
+@admin.register(Patient)
+class PatientAdmin(admin.ModelAdmin):
+    list_display = ("user", "gender", "date_of_birth", "rank", "office_of_assignment")
+    search_fields = ("user__username", "user__email", "rank", "office_of_assignment")
+    list_filter = ("gender", "rank")
+    readonly_fields = ("user",)
+
+
+@admin.register(Dentist)
+class DentistAdmin(admin.ModelAdmin):
+    list_display = ("user", "rank")
+    search_fields = ("user__username", "user__email", "rank")
+    list_filter = ("rank",)
+    readonly_fields = ("user",)
+
+
+@admin.register(MedicalRecord)
+class MedicalRecordAdmin(admin.ModelAdmin):
+    list_display = ("patient", "date_recorded", "diagnosis", "barangay")
+    search_fields = ("patient__user__username", "patient__user__email", "diagnosis")
+    list_filter = ("date_recorded",)
+    readonly_fields = ("date_recorded",)
+
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = (
+        "patient",
+        "city_or_municipality",
+        "province",
+        "region",
+        "postal_code",
+    )
+    search_fields = (
+        "patient__user__username",
+        "patient__user__email",
+        "city_or_municipality",
+        "province",
+    )
+    list_filter = ("region", "province")
