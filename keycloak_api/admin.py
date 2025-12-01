d25758089aeb191277bfff2d16ce96744e7fce31
Rhay from WSL <javier.johnrhay@gmail.com>
2025-12-01 09:14:05 +0800

    Add KeycloakUserAdmin for Django admin interface

--- keycloak_api/admin.py
+++ keycloak_api/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import KeycloakUser
 
-# Register your models here.
+
+@admin.register(KeycloakUser)
+class KeycloakUserAdmin(admin.ModelAdmin):
+	list_display = (
+		"username",
+		"keycloak_id",
+		"email",
+		"given_name",
+		"family_name",
+		"is_staff",
+		"is_active",
+		"updated_at",
+	)
+	search_fields = ("username", "email", "keycloak_id", "given_name", "family_name")
+	list_filter = ("is_staff", "is_active")
+	readonly_fields = ("keycloak_id", "created_at", "updated_at")
