83a48b3ec72c848ecb0d7ee46ffe7bbd4d21710b
Rhay from WSL <javier.johnrhay@gmail.com>
2025-12-01 14:31:29 +0800

    Add MeSerializer to include patient and dentist profiles in KeycloakUser serialization

--- keycloak_api/serializers.py
+++ keycloak_api/serializers.py
@@ -13,3 +13,33 @@ class KeycloakUserSerializer(serializers.ModelSerializer):
             "family_name",
             "roles",
         ]
+
+
+class MeSerializer(serializers.ModelSerializer):
+    patient_profile = serializers.SerializerMethodField()
+    dentist_profile = serializers.SerializerMethodField()
+
+    class Meta:
+        model = KeycloakUser
+        fields = [
+            "keycloak_id",
+            "username",
+            "email",
+            "given_name",
+            "family_name",
+            "roles",
+            "patient_profile",
+            "dentist_profile",
+        ]
+
+    def get_patient_profile(self, obj):
+        if hasattr(obj, "patient_profile"):
+            from dental_records.serializers import PatientSerializer
+            return PatientSerializer(obj.patient_profile).data
+        return None
+
+    def get_dentist_profile(self, obj):
+        if hasattr(obj, "dentist_profile"):
+            from dental_records.serializers import DentistSerializer
+            return DentistSerializer(obj.dentist_profile).data
+        return None
