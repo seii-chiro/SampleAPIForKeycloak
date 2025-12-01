d7da8659f4f791bb9ce4375b79de3c832c547586
Rhay from WSL <javier.johnrhay@gmail.com>
2025-12-01 09:14:16 +0800

    Refactor URL configuration to include UserViewset and improve readability

--- keycloak_api/urls.py
+++ keycloak_api/urls.py
@@ -1,5 +1,18 @@
 from django.urls import path
-from .views import TestView, LoginView, CallbackView, LogoutView, CustomLoginView, AdminView, StandardUserView
+from .views import (
+    TestView,
+    LoginView,
+    CallbackView,
+    LogoutView,
+    CustomLoginView,
+    AdminView,
+    StandardUserView,
+    UserViewset,
+)
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r"users", UserViewset, basename="user")
 
 urlpatterns = [
     path("test/", TestView.as_view(), name="test"),
@@ -9,4 +22,4 @@ urlpatterns = [
     path("logout/", LogoutView.as_view(), name="logout"),
     path("admin_test/", AdminView.as_view(), name="admin"),
     path("standard_user_test/", StandardUserView.as_view(), name="standard_user"),
-]
+] + router.urls
