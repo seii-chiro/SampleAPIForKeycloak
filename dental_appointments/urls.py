07df707b56e3b8f99d09b810dd3b8eb38013cc25
Rhay from WSL <javier.johnrhay@gmail.com>
2025-12-04 08:57:26 +0800

    Register DentalAppointmentStatusViewSet in the URLs

--- dental_appointments/urls.py
+++ dental_appointments/urls.py
@@ -1,8 +1,9 @@
 from rest_framework.routers import DefaultRouter
-from .views import DentalAppointmentViewSet
+from .views import DentalAppointmentViewSet, DentalAppointmentStatusViewSet 
 
 router = DefaultRouter()
 
 router.register("appointments", DentalAppointmentViewSet)
+router.register("appointment-status", DentalAppointmentStatusViewSet)
 
 urlpatterns = router.urls
